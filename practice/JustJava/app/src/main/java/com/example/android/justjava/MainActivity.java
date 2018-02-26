068ff05d2e272b4f1deb7070118f35a15d33e23c
sabdar <developer.sabdar@gmail.com>
2018-02-26 18:25:30 +0530

    JUST JAVA PRACTICE APP UPDATED CODE

--- practice/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ practice/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * IMPORTANT: Make sure you are using the correct package name. 
+ * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
  * package com.example.android.justjava
  * If you get an error when copying this code into Android studio, update it to match teh package name found
@@ -9,13 +9,10 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -25,64 +22,77 @@ public class MainActivity extends AppCompatActivity {
     /**
      * variables quantity and price created and initialized
      */
-    private int quantity =0;
-    private int price = 5;
+    private int quantity = 0;
+    private int cost = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        displayPrice(quantity*price);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        String message = "Total: $ " + number;
-        message += "\nThank You!";
-        displayMessage(message);
-    }
-
     /**
      * This method to increase Quantity
      * @param view
      */
-    public void increment(View view){
+    public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method to decrease quantity
      */
-    public void decrement(View view){
-        if(quantity == 0)
+    public void decrement(View view) {
+        if (quantity == 0)
             return;
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView =  findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice();
+        String message = createOrderSummary(price);
+        displayMessage(message);
+    }
 
+    /**
+     * Calculates the price of the order.
+     * @return total price
+     */
+    private int calculatePrice() {
+        return  quantity * cost;
+    }
+
+    /**
+     * Create Order Summary
+     * @param price of the order
+     * @return summary
+     */
+    private String createOrderSummary(int price) {
+        String message = "Name : Sabdar Shaik";
+        message += "\nQuantity :"+ quantity;
+        message += "\nTotal: $ " + price;
+        message += "\nThank You!";
+        return message;
+    }
 }
