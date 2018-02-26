98a9a91335ca78c71f0aa19c0c59accf7ac5e7fa
sabdar <developer.sabdar@gmail.com>
2018-02-26 18:52:19 +0530

    Cookies Application Completed

--- practice/Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ practice/Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.android.cookies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.status_text_view);
+        imageView = findViewById(R.id.android_cookie_image_view);
+    }
+
+    public void eatCookie(View view){
+        imageView.setImageResource(R.drawable.after_cookie);
+        textView.setText(R.string.status_full_message);
     }
 }
